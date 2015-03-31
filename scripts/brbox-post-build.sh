a9a6547dd359efe9b35c18d51c66dc123f1bffc8
hackboxguy <albert.david@gmail.com>
2015-03-31 07:31:06 +0200

    brbox: fixed brbox-post-build.sh not to override buildroot passed version and build numbers

--- scripts/brbox-post-build.sh
+++ scripts/brbox-post-build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #this script gets called by buildroot after building everything(but before creating rootf.tar)
 #here we are copying version information to /boot/version-num.txt of the target file system
-BRBOX_RELVERSION=00.01 #default values incase if make was called without rel-ver
-BRBOX_BUILDNUM=12345   #default values incase if make was called without build-num
+#BRBOX_RELVERSION=00.01 #default values incase if make was called without rel-ver
+#BRBOX_BUILDNUM=12345   #default values incase if make was called without build-num
 echo "$BRBOX_RELVERSION.$BRBOX_BUILDNUM" > ${TARGET_DIR}/boot/version-num.txt
 
