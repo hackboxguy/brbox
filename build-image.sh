5737d0ac6475a4f67f3ed6d30ead8a0924a0f356
hackboxguy <albert.david@gmail.com>
2015-04-02 11:41:45 +0200

    brbox: fixed baytrail board-system-config re-structuring problem

--- build-image.sh
+++ build-image.sh
@@ -2,8 +2,11 @@
 #this script builds the builroot based image
 BR_FOLDER=./buildroot
 IMAGE_VERSION=XX.YY
-BR_BOARD_CONFIG=baytrail/qemu_x86_64_defconfig
-BR_BOARD_LINUX_CONFIG=baytrail/linux.config 
+
+BR_BOARD_SYSTEM_CONFIG=baytrail
+BR_BOARD_CONFIG=qemu_x86_64_defconfig
+BR_BOARD_LINUX_CONFIG=linux.config 
+
 RAW_DISK_IMAGE=./raw-images/disk.grub1.img.xz
 GRUB1_PREPARE_SCRIPT=grub1-prepare.sh
 GRUB1_COPY_SCRIPT=grub1-copy.sh
@@ -12,17 +15,19 @@ GRUB2_DISK_CREATOR=grub2-bootdisk.sh
 IMAGE_ONLY=0
 PREPARE_ONLY=0
 ###############################################################################
-while getopts o:v:c:s:ip f
+while getopts b:o:v:c:s:ip f
 do
     case $f in
-	o) BR_OUTPUT_FOLDER=$OPTARG;;  #buildroot output folder
-	v) IMAGE_VERSION=$OPTARG ;;    #image version
-	s) SUDOPW=$OPTARG ;;           #sud password
-	i) IMAGE_ONLY=1 ;;             #-i can be used only for binary image creation(build will be skipped)
-        p) PREPARE_ONLY=1 ;;            #just prepare buildroot, dont build
+	b) BR_BOARD_SYSTEM_CONFIG=$OPTARG;;  #board/system type for different application
+	o) BR_OUTPUT_FOLDER=$OPTARG;;        #buildroot output folder
+	v) IMAGE_VERSION=$OPTARG ;;          #image version
+	s) SUDOPW=$OPTARG ;;                 #sud password
+	i) IMAGE_ONLY=1 ;;                   #-i can be used only for binary image creation(build will be skipped)
+        p) PREPARE_ONLY=1 ;;                 #just prepare buildroot, dont build
     esac
 done
 
+#TODO: switch case for br-board-system-config
 
 svn up > /dev/null
 BUILDNUMBER=$(LANG=en_US; svn info 2>&1 | grep "Last Changed Rev:" | sed -e 's/.* //')
@@ -33,9 +38,10 @@ BUILDNUMBER=$(printf "$IMAGE_VERSION.%05d" $BUILDNUMBER)
 BR_OUTPUT_FOLDER=$BR_OUTPUT_FOLDER/$IMAGE_VERSION
 if [ $IMAGE_ONLY = 0 ]; then
 	mkdir -p $BR_OUTPUT_FOLDER
-	cp configs/$BR_BOARD_CONFIG         $BR_FOLDER/configs/
-	cp configs/$BR_BOARD_LINUX_CONFIG   $BR_FOLDER/board/qemu/x86_64/
-	cp configs/busybox.config           $BR_FOLDER/package/busybox/
+	cp configs/$BR_BOARD_SYSTEM_CONFIG/$BR_BOARD_CONFIG         $BR_FOLDER/configs/
+	cp configs/$BR_BOARD_SYSTEM_CONFIG/$BR_BOARD_LINUX_CONFIG   $BR_FOLDER/board/qemu/x86_64/
+	cp configs/$BR_BOARD_SYSTEM_CONFIG/busybox.config           $BR_FOLDER/package/busybox/
+	#TODO: configs/$BR_BOARD_SYSTEM_CONFIG/uclibc.config        $BR_FOLDER/package/uclibc/
 	pushd .
 	cd $BR_FOLDER
 	make O=$BR_OUTPUT_FOLDER $BR_BOARD_CONFIG BRBOX_RELVERSION=$IMAGE_VERSION BRBOX_BUILDNUM=$TMP_BUILDNUM
