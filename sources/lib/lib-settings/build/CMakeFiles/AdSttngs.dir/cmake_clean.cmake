ae04b8791c49d3f7ac7e7ebcf921c282500f5ecb
hackboxguy <albert.david@gmail.com>
2015-04-06 15:17:14 +0200

    brbox: added cmake generated files of libAdSttngs

---
+++ sources/lib/lib-settings/build/CMakeFiles/AdSttngs.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/AdSttngs.dir/src/strmap.cpp.o"
+  "CMakeFiles/AdSttngs.dir/src/settings.cpp.o"
+  "libAdSttngs.pdb"
+  "libAdSttngs.so"
+  "libAdSttngs.so.0.1.1"
+  "libAdSttngs.so.0"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/AdSttngs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
