46d5cba73cfe594d380f8da145288a9436341f3c
hackboxguy <albert.david@gmail.com>
2015-04-19 06:29:45 +0200

    brbox: added common cmake folder

---
+++ sources/cmake/toolchain-arm-backup.cmake
@@ -0,0 +1,17 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+#SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /opt/arm-backup/usr/bin/arm-buildroot-linux-uclibcgnueabi-gcc)
+SET(CMAKE_CXX_COMPILER /opt/arm-backup/usr/bin/arm-buildroot-linux-uclibcgnueabi-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /opt/arm-backup /opt/arm-backup/usr/arm-buildroot-linux-uclibcgnueabi/sysroot)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
