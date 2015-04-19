05bdbcbdf169ab25ceeddcee5fc249ee269eb0f2
hackboxguy <albert.david@gmail.com>
2015-04-19 08:28:20 +0200

    brbox: added sysmgr services source tree

---
+++ sources/services/sysmgr/clt/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include "cmdline.hpp"
+using namespace std;
+int main(int argc, const char* argv[])
+{
+	//SdpcCmdline CmdLine;
+	//CmdLine.parse_cmdline_arguments(argc,(char**)argv);
+	return 0;
+}
