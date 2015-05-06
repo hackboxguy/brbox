0fcbf2f260062a933255003503e670397e9b5450
hackboxguy <albert.david@gmail.com>
2015-05-06 18:33:35 +0200

    brbox: smarteye is more robust for detecting identify pattern

--- sources/services/smarteye/srv/src/ImgIdentify.cpp
+++ sources/services/smarteye/srv/src/ImgIdentify.cpp
@@ -29,14 +29,17 @@ int ImgIdentify::find_relative_square(const vector<vector <Point> >& squares, ve
 	const int n_points = 4;
 
 	Rect relative_rect = boundingRect(Mat(biggest_square));
+	cout<<"rel.width="<<relative_rect.width<<" rel.height="<<relative_rect.height<<endl;
 	relative_rect.width/=scale_factor;
 	relative_rect.height/=scale_factor;
+
 	for (size_t i = 0; i < squares.size(); i++) 
 	{
 		Rect rectangle = boundingRect(Mat(squares[i]));
 		if(rectangle.width<MIN_WIDTH_THRESHOLD)//ignore too small squares
 			continue;
 
+//cout<<"i="<<i<<" : rect.width="<<rectangle.width<<" : rect.height="<<rectangle.height<<endl;
 		//check if small rect fits within the tolerence window
 		if( (rectangle.width >= relative_rect.width-tolerence) && (rectangle.width <= relative_rect.width+tolerence) )
 		{
@@ -45,7 +48,40 @@ int ImgIdentify::find_relative_square(const vector<vector <Point> >& squares, ve
 			if( (rectangle.height >= relative_rect.height-tolH) && (rectangle.height <= relative_rect.height+tolH) )
 			{
 				relative_square = squares[i];
-				//cout<<"found smaller rect"<<endl;
+				return 0;
+			}
+		}
+	}
+	return -1;
+}
+int ImgIdentify::find_relative_square_new(const vector<vector <Point> >& squares, vector<Point> biggest_square,vector<Point>& relative_square,float scale_W,float scale_H,float tolerence) 
+{
+	if (!squares.size()) 
+		return -1;
+	int max_width = 0;
+	int max_height = 0;
+	int max_square_idx = 0;
+	const int n_points = 4;
+
+	float tmp_width,tmp_height;
+
+	Rect relative_rect = boundingRect(Mat(biggest_square));
+//cout<<"ref.width="<<relative_rect.width<<" ref.height="<<relative_rect.height<<endl;
+
+	for (size_t i = 0; i < squares.size(); i++) 
+	{
+		Rect rectangle = boundingRect(Mat(squares[i]));
+		if(rectangle.width<MIN_WIDTH_THRESHOLD)//ignore too small squares
+			continue;
+
+		tmp_width =(float)relative_rect.width/rectangle.width;
+		tmp_height=(float)relative_rect.height/rectangle.height;
+//cout<<"tmpW="<<tmp_width<<" tmpH="<<tmp_height<<" W="<<rectangle.width<<" H="<<rectangle.height<<endl;
+		if( (tmp_width >= (float)(scale_W-tolerence) ) && (tmp_width <= (float)(scale_W+tolerence)) )
+		{
+			if( (tmp_height >= (float)(scale_H-tolerence) ) && (tmp_height <= (float)(scale_H+tolerence)) )
+			{
+				relative_square = squares[i];
 				return 0;
 			}
 		}
@@ -54,7 +90,7 @@ int ImgIdentify::find_relative_square(const vector<vector <Point> >& squares, ve
 }
 /*---------------------------------------------------------------------------*/
 //finds the larges square from squares-list
-int ImgIdentify::find_largest_square(const vector<vector <Point> >& squares, vector<Point>& biggest_square) 
+int ImgIdentify::find_largest_square(const vector<vector <Point> >& squares, vector<Point>& biggest_square,float ratioLow,float ratioHigh) 
 {
 	if (!squares.size()) 
 		return -1;
@@ -64,10 +100,13 @@ int ImgIdentify::find_largest_square(const vector<vector <Point> >& squares, vec
 	int max_square_idx = 0;
 	const int n_points = 4;
 
+	float WHratio;
+
 	for (size_t i = 0; i < squares.size(); i++) 
 	{
 		Rect rectangle = boundingRect(Mat(squares[i]));
-		if ((rectangle.width >= max_width) && (rectangle.height >= max_height)) 
+		WHratio=(float)((float)rectangle.width/(float)rectangle.height);
+		if ((rectangle.width >= max_width) && (rectangle.height >= max_height) && (WHratio>=ratioLow) && (WHratio<=ratioHigh)) 
 		{
 			max_width = rectangle.width;
 			max_height = rectangle.height;
@@ -110,6 +149,7 @@ int ImgIdentify::apply_threshold(Mat& src_gray,int trsh,RNG &rng,Mat& drawing, v
 		rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
 		//circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
 	}
+//cout<<"ImgIdentify::apply_threshold:threshold="<<trsh<<" squares="<<contours.size()<<endl;
 	return 0;
 }
 /*---------------------------------------------------------------------------*/
@@ -141,7 +181,7 @@ RPC_SRV_RESULT ImgIdentify::capture_camera_image(int video_device,int frmPixels,
 	return RPC_SRV_RESULT_SUCCESS;//0;
 }
 /*---------------------------------------------------------------------------*/
-RPC_SRV_RESULT ImgIdentify::identify_image_box(/*bool logImage,*/std::string imgPath)
+RPC_SRV_RESULT ImgIdentify::identify_image_box(std::string imgPath)
 {
 	Mat image,src_gray;
 	RPC_SRV_RESULT res;
@@ -176,12 +216,20 @@ RPC_SRV_RESULT ImgIdentify::identify_image_box(/*bool logImage,*/std::string img
 	vector<Point> biggest_square;
 	vector<Point> smaller_square;
 	//find largest square
-	find_largest_square(contours/*squares*/,biggest_square);//find outer white box of ovd/kvd identify pattern
+	find_largest_square(contours/*squares*/,biggest_square,1.5,1.8);//find outer white box of ovd/kvd identify pattern
 	//find a smaller square which is 3times smaller than largest
-	if(find_relative_square(contours/*squares*/,biggest_square,smaller_square,3,20)==0)//; //+/- 20pixel tolerence
+
+//	if(find_relative_square(contours/*squares*/,biggest_square,smaller_square,3,30)==0)//; //+/- 20pixel tolerence
+//		res=RPC_SRV_RESULT_SUCCESS;
+//	else
+//		res=RPC_SRV_RESULT_FAIL;//required image is not found
+
+	if(find_relative_square_new(contours, biggest_square,smaller_square,3.15,3.25,0.8)==0)
 		res=RPC_SRV_RESULT_SUCCESS;
 	else
 		res=RPC_SRV_RESULT_FAIL;//required image is not found
+
+
 
 	//check if user requested to log the countour image
 	if(imgPath!="")
@@ -190,12 +238,18 @@ RPC_SRV_RESULT ImgIdentify::identify_image_box(/*bool logImage,*/std::string img
 	return res;
 }
 /*---------------------------------------------------------------------------*/
+RPC_SRV_RESULT ImgIdentify::identify_color_box(std::string imgPath,EJSON_SMARTEYE_IFACE_TYPE pattern)
+{
+
+	return RPC_SRV_RESULT_SUCCESS;
+}
+/*---------------------------------------------------------------------------*/
 RPC_SRV_RESULT ImgIdentify::IdentifyPattern(EJSON_SMARTEYE_IFACE_TYPE pattern,std::string imgPath)
 {
 	switch(pattern)
 	{
 		case EJSON_SMARTEYE_IDPATTERN_IDENT:return identify_image_box(imgPath);//"/home/adav/tmp/adav-cam.jpg");
-		case EJSON_SMARTEYE_IDPATTERN_RED  :
+		case EJSON_SMARTEYE_IDPATTERN_RED  :return identify_color_box(imgPath,EJSON_SMARTEYE_IDPATTERN_RED);
 		case EJSON_SMARTEYE_IDPATTERN_GREEN:
 		case EJSON_SMARTEYE_IDPATTERN_BLUE :
 		case EJSON_SMARTEYE_IDPATTERN_WHITE:
--- sources/services/smarteye/srv/src/ImgIdentify.h
+++ sources/services/smarteye/srv/src/ImgIdentify.h
@@ -8,15 +8,17 @@
 #include "SmarteyeJsonDef.h"
 using namespace std;
 using namespace cv;
-#define MIN_WIDTH_THRESHOLD 50 //ignore squares with too small width
-#define MAX_SQARES_FILTER   20 //when too many contours are found, then increase the threshold and scan again.
+#define MIN_WIDTH_THRESHOLD 100 //ignore squares with too small width
+#define MAX_SQARES_FILTER   10 //when too many contours are found, then increase the threshold and scan again.
 #define MIN_SCAN_THRESHOLD  100 //start with 100, can go upto 200
 /*---------------------------------------------------------------------------*/
 class  ImgIdentify
 {
 	double angle(Point pt1, Point pt2, Point pt0);
 	int find_relative_square(const vector<vector <Point> >& squares, vector<Point> biggest_square,vector<Point>& relative_square,int scale_factor,int tolerence);
-	int find_largest_square(const vector<vector <Point> >& squares, vector<Point>& biggest_square);
+	int find_relative_square_new(const vector<vector <Point> >& squares, vector<Point> biggest_square,vector<Point>& relative_square,float scale_W,float scale_H,float tolerence);
+
+	int find_largest_square(const vector<vector <Point> >& squares, vector<Point>& biggest_square,float ratioLow,float ratioHigh);
 	int apply_threshold(Mat& src_gray,int trsh,RNG &rng,Mat& drawing, vector<vector<Point> > &contours);
 	int scale_image(Mat& image, const float imgScaleFactor);
 	RPC_SRV_RESULT capture_camera_image(int video_device,int frmPixels,int frmLines,Mat& image);
@@ -26,6 +28,7 @@ public:
 	 ImgIdentify();
 	~ ImgIdentify();
 	RPC_SRV_RESULT IdentifyPattern(EJSON_SMARTEYE_IFACE_TYPE pattern,std::string imgPath);
+	RPC_SRV_RESULT identify_color_box(std::string imgPath,EJSON_SMARTEYE_IFACE_TYPE pattern);
 
 };
 /*---------------------------------------------------------------------------*/
