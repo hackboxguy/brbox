2b1829ce9b70f6a2685970fa36c06e159709eb00
hackboxguy <albert.david@gmail.com>
2015-05-15 16:02:36 +0200

    brbox: baytrail-update package as per new cmake

--- packages/brbox_baytrail_update/brbox_baytrail_update.mk
+++ packages/brbox_baytrail_update/brbox_baytrail_update.mk
@@ -3,47 +3,9 @@
 # brbox baytrail update scripts
 #
 #############################################################
+BRBOX_BAYTRAIL_UPDATE_SITE_METHOD = local
+BRBOX_BAYTRAIL_UPDATE_SITE = $(TOPDIR)/../sources/scripts/baytrail_update
+BRBOX_BAYTRAIL_UPDATE_INSTALL_STAGING = NO
+BRBOX_BAYTRAIL_UPDATE_INSTALL_TARGET = YES
+$(eval $(cmake-package))
 
-BRBOX_BAYTRAIL_UPDATE_DIR:=$(BUILD_DIR)/brbox_baytrail_update
-
-# Strip off the annoying quoting
-#BRBOX_BAYTRAIL_UPDATE_SRC:=$(call qstrip,$(BR2_PACKAGE_BRBOX_BAYTRAIL_UPDATE_SOURCE))
-BRBOX_BAYTRAIL_UPDATE_SRC:=$(call qstrip,$(BR2_TARGET_BRBOX_SVNURL))/sources/scripts/baytrail_update
-
-$(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_downloaded:
-	mkdir -p $(BUILD_DIR)
-	svn co $(BRBOX_BAYTRAIL_UPDATE_SRC) $(BRBOX_BAYTRAIL_UPDATE_DIR)
-	touch $@
-
-$(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_extracted: $(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_downloaded
-	touch $@
-
-$(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_configured: $(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_extracted
-	touch $@
-
-$(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_built: $(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_configured
-	touch $@
-
-$(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_target_installed: $(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_built
-	mkdir -p $(TARGET_DIR)/usr/sbin
-	cp -a $(BRBOX_BAYTRAIL_UPDATE_DIR)/baytrail-brdskmgr $(TARGET_DIR)/usr/sbin/brdskmgr
-	cp -a $(BRBOX_BAYTRAIL_UPDATE_DIR)/baytrail-update   $(TARGET_DIR)/usr/sbin/update
-	touch $@
-
-brbox_baytrail_update: $(BRBOX_BAYTRAIL_UPDATE_DIR)/.stamp_target_installed
-
-brbox_baytrail_update-clean:
-	rm -f $(TARGET_DIR)/usr/bin/update
-	rm -f $(TARGET_DIR)/usr/bin/brdskmgr
-	rm -f .stamp_*_installed
-
-brbox_baytrail_update-dirclean:
-	rm -rf $(BRBOX_BAYTRAIL_UPDATE_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_BRBOX_BAYTRAIL_UPDATE),y)
-TARGETS+=brbox_baytrail_update
-endif
