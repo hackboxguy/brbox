ec5191c4b999bb475bf0c60d9655414cdd3c44e3
hackboxguy <albert.david@gmail.com>
2015-05-17 05:43:22 +0200

    brbox: added a switchable flag in package file to enable/disable auto-svn-revision-include

--- packages/brbox_mkimage/brbox_mkimage.mk
+++ packages/brbox_mkimage/brbox_mkimage.mk
@@ -3,11 +3,12 @@
 # mkimage for target
 #
 #############################################################
+BRBOX_MKIMAGE_VERSION = 0.1
 BRBOX_MKIMAGE_SITE_METHOD = local
 BRBOX_MKIMAGE_SITE = $(TOPDIR)/../sources/utils/brbox-mkimage
 BRBOX_MKIMAGE_INSTALL_STAGING = NO
 BRBOX_MKIMAGE_INSTALL_TARGET = YES
-BRBOX_MKIMAGE_CONF_OPTS=-DMKIMG_HOST_BUILD=OFF
+BRBOX_MKIMAGE_CONF_OPTS=-DMKIMG_HOST_BUILD=OFF -DAUTO_SVN_VERSION=OFF #todo: when SITE_METHOD is svnco, enable AUTO_SVN_VERSION flag
 #BRBOX_MKIMAGE_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/opt/fmw/"
 $(eval $(cmake-package))
 
--- packages/brbox_mkimage/host-brbox_mkimage.mk
+++ packages/brbox_mkimage/host-brbox_mkimage.mk
@@ -3,11 +3,12 @@
 # mkimage for host
 #
 #############################################################
+BRBOX_MKIMAGE_VERSION = 0.1
 BRBOX_MKIMAGE_SITE_METHOD = local
 BRBOX_MKIMAGE_SITE = $(TOPDIR)/../sources/utils/brbox-mkimage
 BRBOX_MKIMAGE_INSTALL_STAGING = NO
 BRBOX_MKIMAGE_INSTALL_TARGET = NO
-#BRBOX_MKIMAGE_CONF_OPTS=-DMKIMG_HOST_BUILD="ON"
+HOST_BRBOX_MKIMAGE_CONF_OPTS=-DAUTO_SVN_VERSION=OFF #todo: when SITE_METHOD is svnco, enable this flag
 #BRBOX_MKIMAGE_CONF_OPT=-DCMAKE_INSTALL_PREFIX="opt/bin"
 $(eval $(host-cmake-package))
 
