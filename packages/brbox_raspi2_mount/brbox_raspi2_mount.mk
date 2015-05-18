52dc69ebd4ce7142394c2c9f8a4398c254f5719e
hackboxguy <albert.david@gmail.com>
2015-05-18 05:15:19 +0200

    brbox: seperated S02Mount startup script as seperate package for raspi2

---
+++ packages/brbox_raspi2_mount/brbox_raspi2_mount.mk
@@ -0,0 +1,12 @@
+#############################################################
+#
+# brbox raspi2 mount startup-script
+#
+#############################################################
+BRBOX_RASPI2_MOUNT_SITE_METHOD = local
+BRBOX_RASPI2_MOUNT_SITE = $(TOPDIR)/../sources/scripts/raspi2/boot
+BRBOX_RASPI2_MOUNT_INSTALL_STAGING = NO
+BRBOX_RASPI2_MOUNT_INSTALL_TARGET = YES
+BRBOX_RASPI2_MOUNT_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/etc/"
+$(eval $(cmake-package))
+
