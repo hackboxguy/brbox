e19958105cfea34e94934ba5b78d4e8c5c88ca3c
hackboxguy <albert.david@gmail.com>
2015-05-21 06:49:22 +0200

    brbox: updated i210kmodAvbIgb package

--- packages/brbox_I210kmodAvbIgb/brbox_I210kmodAvbIgb.mk
+++ packages/brbox_I210kmodAvbIgb/brbox_I210kmodAvbIgb.mk
@@ -1,8 +1,24 @@
-BRBOX_LIBADCMN_VERSION = 0.1
-BRBOX_LIBADCMN_SITE_METHOD = local
-BRBOX_LIBADCMN_SITE = $(TOPDIR)/../sources/lib/lib-adav
-BRBOX_LIBADCMN_INSTALL_STAGING = YES
-BRBOX_LIBADCMN_INSTALL_TARGET = YES
-BRBOX_LIBADCMN_DEPENDENCIES = brbox_libAdSttngs json-c
-$(eval $(cmake-package))
+################################################################################
+#
+# Intel i210 igb_avb module
+#
+################################################################################
+BARCO_I210KMODAVBIGB_SRC:=$(call qstrip,$("http://github.com/AVnu/Open-AVB/tree/master/kmod/igb"))
+BARCO_I210KMODAVBIGB_VERSION=HEAD
+BARCO_I210KMODAVBIGB_SITE_METHOD=svn
+BARCO_I210KMODAVBIGB_SITE=$(BARCO_I210KMODAVBIGB_SRC)
+BARCO_I210KMODAVBIGB_INSTALL_STAGING=NO
+BARCO_I210KMODAVBIGB_INSTALL_TARGET=YES
+BRBOX_I210KMODAVBIGB_LICENSE = GPLv2+
+BARCO_I210KMODAVBIGB_DEPENDENCIES=linux
+#There is no license file
+#define ON2_8170_MODULES_BUILD_CMDS
+#	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D)
+#endef
+#define ON2_8170_MODULES_INSTALL_TARGET_CMDS
+#	$(MAKE) -C $(LINUX_DIR) $(LINUX_MAKE_FLAGS) M=$(@D) modules_install
+#endef
+$(eval $(generic-package))
+
+
 
