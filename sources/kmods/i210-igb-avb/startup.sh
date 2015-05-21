197f8c86df37a1a56ade5f7fade417ebde174957
hackboxguy <albert.david@gmail.com>
2015-05-21 07:39:29 +0200

    brbox: adding open-avb-igb sources

---
+++ sources/kmods/i210-igb-avb/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Network interface name (e.g. Fedora=p2p1, Ubuntu=eth2, OpenSUSE=ens01)
+INTERFACE=p2p1
+export INTERFACE
+
+rmmod igb
+rmmod igb_avb
+insmod ./igb_avb.ko
+sleep 1
+ifconfig $INTERFACE down
+echo 0 > /sys/class/net/$INTERFACE/queues/tx-0/xps_cpus
+echo 0 > /sys/class/net/$INTERFACE/queues/tx-1/xps_cpus
+echo f > /sys/class/net/$INTERFACE/queues/tx-2/xps_cpus
+echo f > /sys/class/net/$INTERFACE/queues/tx-3/xps_cpus
+ifconfig $INTERFACE up
