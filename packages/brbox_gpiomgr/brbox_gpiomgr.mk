2aa631dc5391d71b492b2fb60965abda01267fee
hackboxguy <albert.david@gmail.com>
2015-05-27 06:46:51 +0200

    brbox:added gpiomgr buildroot-package

---
+++ packages/brbox_gpiomgr/brbox_gpiomgr.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# gpiomgr (gpio control server)
+#
+#############################################################
+BRBOX_GPIOMGR_VERSION = 0.1
+BRBOX_GPIOMGR_SITE_METHOD = local
+BRBOX_GPIOMGR_SITE = $(TOPDIR)/../sources/services/gpioctl
+BRBOX_GPIOMGR_INSTALL_STAGING = YES
+BRBOX_GPIOMGR_INSTALL_TARGET = YES
+BRBOX_GPIOMGR_DEPENDENCIES = json-c brbox_libAdCmnOld brbox_libAdSttngs
+BRBOX_GPIOMGR_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/opt/fmw/" -DAUTO_SVN_VERSION=OFF -DINSTALL_MY_HEADERS=OFF
+$(eval $(cmake-package))
+
