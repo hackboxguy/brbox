92786a4e41ee50e260778e5103fe5cc62700af62
hackboxguy <albert.david@gmail.com>
2015-07-21 16:35:23 +0200

    brbox: fixed http-avahi-publish-service startup script

--- scripts/brbox-post-build.sh
+++ scripts/brbox-post-build.sh
@@ -4,4 +4,5 @@
 #BRBOX_RELVERSION=00.01 #default values incase if make was called without rel-ver
 #BRBOX_BUILDNUM=12345   #default values incase if make was called without build-num
 echo "$BRBOX_RELVERSION.$BRBOX_BUILDNUM" > ${TARGET_DIR}/boot/version-num.txt
+rm -rf ${TARGET_DIR}/etc/avahi/services/http.service
 ln -s /tmp/http.service ${TARGET_DIR}/etc/avahi/services/http.service 
