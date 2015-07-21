92172162271d67656f83d89f3a65184b482c437b
hackboxguy <albert.david@gmail.com>
2015-07-21 16:14:06 +0200

    brbox: created http-avahi-servie symbolic link in tmp

--- scripts/brbox-post-build.sh
+++ scripts/brbox-post-build.sh
@@ -4,4 +4,4 @@
 #BRBOX_RELVERSION=00.01 #default values incase if make was called without rel-ver
 #BRBOX_BUILDNUM=12345   #default values incase if make was called without build-num
 echo "$BRBOX_RELVERSION.$BRBOX_BUILDNUM" > ${TARGET_DIR}/boot/version-num.txt
-
+ln -s /tmp/http.service ${TARGET_DIR}/etc/avahi/services/http.service 
