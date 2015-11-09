e0a088db8ff0e15f63a2ba758ec0f818ba689fb8
hackboxguy <albert.david@gmail.com>
2015-11-09 07:04:26 +0100

    brbox: updated bbb config to point to correct post build script

--- scripts/brbox-post-build.sh
+++ scripts/brbox-post-build.sh
@@ -3,10 +3,10 @@
 #here we are copying version information to /boot/version-num.txt of the target file system
 #BRBOX_RELVERSION=00.01 #default values incase if make was called without rel-ver
 #BRBOX_BUILDNUM=12345   #default values incase if make was called without build-num
-if [ ! -d "${TARGET_DIR}/boot" ]; then
-	#for bbb /target/boot folder doesnt exists
-	mkdir -p ${TARGET_DIR}/boot
-fi
+#if [ ! -d "${TARGET_DIR}/boot" ]; then
+#	#for bbb /target/boot folder doesnt exists
+#	mkdir -p ${TARGET_DIR}/boot
+#fi
 echo "$BRBOX_RELVERSION.$BRBOX_BUILDNUM" > ${TARGET_DIR}/boot/version-num.txt
 rm -rf ${TARGET_DIR}/etc/avahi/services/http.service
 ln -s /tmp/http.service ${TARGET_DIR}/etc/avahi/services/http.service 
