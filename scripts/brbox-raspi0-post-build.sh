d030a3c167665734e996d7b06a185b46039fc494
hackboxguy <albert.david@gmail.com>
2016-08-31 21:48:51 +0200

    brbox: raspi0: added separate post-build.sh script

---
+++ scripts/brbox-raspi0-post-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#this script gets called by buildroot after building everything(but before creating rootf.tar)
+#here we are copying version information to /boot/version-num.txt of the target file system
+#BRBOX_RELVERSION=00.01 #default values incase if make was called without rel-ver
+#BRBOX_BUILDNUM=12345   #default values incase if make was called without build-num
+#BRBOX_SYSCONFIG application specific distribution e.g:raspi1-rbox or bbbmmc-rbox etc..
+if [ ! -d "${TARGET_DIR}/boot" ]; then
+	#for bbb /target/boot folder doesnt exists
+	mkdir -p ${TARGET_DIR}/boot
+fi
+echo "$BRBOX_RELVERSION.$BRBOX_BUILDNUM" > ${TARGET_DIR}/boot/version-num.txt
+echo "$BRBOX_SYSCONFIG" > ${TARGET_DIR}/boot/sysconfig.txt
+#rm -rf ${TARGET_DIR}/etc/avahi/services/http.service
+#ln -s /tmp/http.service ${TARGET_DIR}/etc/avahi/services/http.service 
