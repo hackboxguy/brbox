d9dcc1607d3e8aeab9375bc45cf4766c0d56e5b4
hackboxguy <albert.david@gmail.com>
2017-07-04 14:27:52 +0200

    brbox: modified lib-onion package to v0.8

--- packages/brbox_libonion/brbox_libonion.mk
+++ packages/brbox_libonion/brbox_libonion.mk
@@ -3,7 +3,7 @@
 # brbox_libonion
 #
 ################################################################################
-BRBOX_LIBONION_VERSION = HEAD #v0.7
+BRBOX_LIBONION_VERSION = v0.8 #HEAD #v0.7
 BRBOX_LIBONION_SITE = $(call github,davidmoreno,onion,$(BRBOX_LIBONION_VERSION))
 BRBOX_LIBONION_INSTALL_STAGING = YES
 BRBOX_LIBONION_CONF_OPTS = -DONION_USE_SSL=false -DONION_USE_PAM=false -DONION_USE_SYSTEMD=false -DONION_USE_SQLITE3=false -DONION_USE_GC=false -DONION_USE_TESTS=false -DONION_EXAMPLES=false -DONION_USE_BINDINGS_CPP=true -DONION_USE_JPEG=false -DONION_USE_PNG=false
