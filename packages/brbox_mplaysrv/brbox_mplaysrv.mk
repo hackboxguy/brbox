ea6eb3997a9b0188ed91e040d73682f7b75ae7ea
hackboxguy <albert.david@gmail.com>
2017-07-18 23:33:25 +0200

    brbox: added brbox_mplaysrv buildroot package

---
+++ packages/brbox_mplaysrv/brbox_mplaysrv.mk
@@ -0,0 +1,15 @@
+#############################################################
+#
+# mplaysrv (media-player server)
+#
+#############################################################
+BRBOX_MPLAYSRV_STARTNUM=$(call qstrip,$(BR2_PACKAGE_BRBOX_MPLAYSRV_INIT_ORDER))
+BRBOX_MPLAYSRV_VERSION = 0.1
+BRBOX_MPLAYSRV_SITE_METHOD = local
+BRBOX_MPLAYSRV_SITE = $(TOPDIR)/../sources/services/mplaysrv #hotwire has been renamed to mplaysrv
+BRBOX_MPLAYSRV_INSTALL_STAGING = YES
+BRBOX_MPLAYSRV_INSTALL_TARGET = YES
+BRBOX_MPLAYSRV_DEPENDENCIES = json-c brbox_libAdCmnOld brbox_libAdSttngs opencv
+BRBOX_MPLAYSRV_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/opt/fmw/" -DAUTO_SVN_VERSION=OFF -DINSTALL_MY_HEADERS=OFF -DSTARTNUM=$(BRBOX_MPLAYSRV_STARTNUM)
+$(eval $(cmake-package))
+
