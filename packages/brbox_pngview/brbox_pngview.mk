961d1a9ad0979b60769ee6bc733c396ef1a90978
hackboxguy <albert.david@gmail.com>
2017-08-22 22:01:27 +0200

    brbox: added pngview utility and buildroot package

---
+++ packages/brbox_pngview/brbox_pngview.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# pngview-app
+#
+################################################################################
+BRBOX_PNGVIEW_VERSION = master
+BRBOX_PNGVIEW_SITE_METHOD = local
+BRBOX_PNGVIEW_SITE = $(TOPDIR)/../sources/utils/pngview
+BRBOX_PNGVIEW_LICENSE = GPL-2.0
+BRBOX_PNGVIEW_LICENSE_FILES = COPYING
+
+define BRBOX_PNGVIEW_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define BRBOX_PNGVIEW_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/pngview $(TARGET_DIR)/usr/bin/pngview
+endef
+
+$(eval $(generic-package))
