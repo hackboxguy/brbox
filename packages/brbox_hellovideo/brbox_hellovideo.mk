d2cc4f85da920277bc4200043cce9785999dda83
hackboxguy <albert.david@gmail.com>
2017-08-29 23:32:36 +0200

    brbox: added buildroot package for libIlClient-library and hello-video-utility

---
+++ packages/brbox_hellovideo/brbox_hellovideo.mk
@@ -0,0 +1,14 @@
+#############################################################
+#
+# hellovideo (rpi video player)
+#
+#############################################################
+BRBOX_HELLOVIDEO_VERSION = 0.1
+BRBOX_HELLOVIDEO_SITE_METHOD = local
+BRBOX_HELLOVIDEO_SITE = $(TOPDIR)/../sources/utils/hello_video
+BRBOX_HELLOVIDEO_INSTALL_STAGING = NO
+BRBOX_HELLOVIDEO_INSTALL_TARGET = YES
+BRBOX_HELLOVIDEO_DEPENDENCIES = brbox_libIlClient
+BRBOX_HELLOVIDEO_CONF_OPTS=-DCMAKE_INSTALL_PREFIX="/opt/fmw/" -DAUTO_SVN_VERSION=OFF -DINSTALL_MY_HEADERS=OFF
+$(eval $(cmake-package))
+
