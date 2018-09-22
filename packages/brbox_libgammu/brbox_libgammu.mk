d9c1d642f182810cbddd3c23014477cbc3bd3f83
hackboxguy <albert.david@gmail.com>
2018-09-22 22:17:32 +0200

    brbox: increased libgammu version to 1.39.0 and disabled lightsensesrv in raspi1-disptst config

--- packages/brbox_libgammu/brbox_libgammu.mk
+++ packages/brbox_libgammu/brbox_libgammu.mk
@@ -3,7 +3,7 @@
 # brbox_libgammu
 #
 ################################################################################
-BRBOX_LIBGAMMU_VERSION = 1.33.0 #1.36.6
+BRBOX_LIBGAMMU_VERSION = 1.39.0 #1.36.6
 BRBOX_LIBGAMMU_SITE = $(call github,gammu,gammu,$(BRBOX_LIBGAMMU_VERSION))
 BRBOX_LIBGAMMU_INSTALL_STAGING = YES
 BRBOX_LIBGAMMU_CONF_OPTS = -DWITH_NOKIA_SUPPORT=OFF -DWITH_BLUETOOTH=OFF -DWITH_IRDA=OFF
