e40a44782b818de17f7d5dd92d6ae6d4c7cc2aa2
hackboxguy <albert.david@gmail.com>
2019-02-11 08:22:10 +0100

    brbox: updated brbox_gstd br package

--- packages/brbox_gstd/brbox_gstd.mk
+++ packages/brbox_gstd/brbox_gstd.mk
@@ -12,9 +12,20 @@ BRBOX_GSTD_GIT_SUBMODULES = YES
 BRBOX_GSTD_LICENSE = LGPL-2.1+
 BRBOX_GSTD_LICENSE_FILES = COPYING
 
-BRBOX_GSTD_AUTORECONF = YES
 BRBOX_GSTD_DEPENDENCIES = host-pkgconf gstreamer1 gst1-plugins-base libglib2 json-glib ncurses readline
-BRBOX_GSTD_CONF_OPTS = --enable-gtk-doc=no
+BRBOX_GSTD_AUTORECONF = YES
+BRBOX_GSTD_CONF_OPTS += --disable-gtk-doc
+
+define BRBOX_GSTD_CONFIGURE_CMDS
+	(cd $(@D) && rm -rf config.cache && \
+	$(TARGET_CONFIGURE_OPTS) \
+	$(TARGET_CONFIGURE_ARGS) \
+	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
+	./configure \
+		--prefix=/usr \
+	)
+endef
+
 
 $(eval $(autotools-package))
 
