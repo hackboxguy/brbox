5fdf309027bf0c749839607e4f73e3a63c3e3cb9
Albert David <albert.david@gmail.com>
2019-03-01 22:12:58 +0100

    brbox: added play-uri.sh script

---
+++ sources/scripts/cmn/gstd-startup-script/play-uri.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#./play-uri.sh -g <gstd_ip> -u <stream_port> -s <videosink>
+USAGE="./play-uri.sh -g <gstd_ip> -u <stream_port> -s <videosink>"
+URI=""
+VIDEOSINK="xvimagesink"
+GSTD_IP=127.0.0.1  #gstreamer-daemon ip
+GSTD_CLIENT_BIN=gstd-client
+
+
+while getopts u:g:s: f
+do
+    case $f in
+	u) URI=$OPTARG ;;
+	g) GSTD_IP=$OPTARG ;;
+	s) VIDEOSINK=$OPTARG ;;
+    esac
+done
+
+if [ $# -lt 2  ]; then 
+	echo $USAGE
+	exit 1
+fi
+
+RES=$($GSTD_CLIENT_BIN --address=$GSTD_IP --port=5000 pipeline_delete p1)
+
+RES=$($GSTD_CLIENT_BIN --address=$GSTD_IP --port=5000 create /pipelines p1 playbin3 uri="$URI" video-sink=$VIDEOSINK)
+
+RES=$($GSTD_CLIENT_BIN --address=$GSTD_IP --port=5000 pipeline_play p1)
+
