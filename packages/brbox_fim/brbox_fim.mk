dc476de838ef5e345bfc69e789ad8cca96ba3544
hackboxguy <albert.david@gmail.com>
2020-02-22 00:09:54 +0100

    brbox: updated fim package

--- packages/brbox_fim/brbox_fim.mk
+++ packages/brbox_fim/brbox_fim.mk
@@ -3,18 +3,15 @@
 # brbox_fbv_improved
 #
 ################################################################################
-BRBOX_FIM_VERSION = 0.2.2
-BRBOX_FIM_SOURCE = fim-$(DMIDECODE_VERSION).tar.bz2
-BRBOX_FIM_SITE = http://download.savannah.gnu.org/releases/fim
+BRBOX_FIM_VERSION = 0.6
+BRBOX_FIM_SOURCE = fim-$(BRBOX_FIM_VERSION)-trunk.tar.gz
+BRBOX_FIM_SITE = http://download.savannah.nongnu.org/releases/fbi-improved
 BRBOX_FIM_LICENSE = GPL-2.0+
 BRBOX_FIM_LICENSE_FILES = LICENSE
-
-define BRBOX_FIM_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS)
-endef
-
-define BRBOX_FIM_INSTALL_TARGET_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) prefix=/usr DESTDIR=$(TARGET_DIR) install
-endef
-
-$(eval $(generic-package))
+BRBOX_FIM_DEPENDENCIES += host-pkgconf libexif libpng ncurses readline flex
+#BRBOX_FIM_CONF_ENV += libpng_config_prefix=$(STAGING_DIR)/usr/bin/libpng-config
+#BRBOX_FIM_CONF_ENV += fim_cv_regex=no
+BRBOX_FIM_CONF_ENV += ac_cv_path_LIBPNG_CONFIG=$(STAGING_DIR)/usr/bin/libpng-config
+BRBOX_FIM_AUTORECONF = YES
+BRBOX_FIM_CONF_OPTS += --enable-libpng --disable-exif 
+$(eval $(autotools-package))
