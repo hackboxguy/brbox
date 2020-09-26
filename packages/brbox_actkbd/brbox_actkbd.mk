3c2103fa78183f7fcc1b7f63f4684ca413a8eb87
Albert David <albert.david@gmail.com>
2020-09-26 23:45:31 +0200

    converted actkbd buildroot package from make to cmake based

--- packages/brbox_actkbd/brbox_actkbd.mk
+++ packages/brbox_actkbd/brbox_actkbd.mk
@@ -4,18 +4,11 @@
 #
 ################################################################################
 BRBOX_ACTKBD_VERSION = master
-BRBOX_ACTKBD_SITE = $(call github,thkala,actkbd,$(BRBOX_ACTKBD_VERSION))
+BRBOX_ACTKBD_SITE = $(call github,hackboxguy,actkbd,$(BRBOX_ACTKBD_VERSION))
 BRBOX_ACTKBD_INSTALL_STAGING = NO
-BRBOX_ACTKBD_DEPENDENCIES = readline hidapi
+BRBOX_ACTKBD_INSTALL_TARGET = YES
 BRBOX_ACTKBD_LICENSE = GPL-2.0
 BRBOX_ACTKBD_LICENSE_FILES = COPYING
+BRBOX_ACTKBD_CONF_OPTS=-DAUTO_SVN_VERSION=OFF
 
-define BRBOX_ACTKBD_BUILD_CMDS
-	$(MAKE) all -C $(@D)
-endef
-
-define BRBOX_ACTKBD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/actkbd $(TARGET_DIR)/usr/bin/actkbd
-endef
-
-$(eval $(generic-package))
+$(eval $(cmake-package))
