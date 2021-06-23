2d43b185b79a82572be78b7a29a2bee7f5ad95f5
Albert David <albert.david@barco.com>
2021-06-23 23:15:40 +0200

    cleaned up of left over disclaimer

--- sources/kmods/genintrdrv/kmod/sl_chain.c
+++ sources/kmods/genintrdrv/kmod/sl_chain.c
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2009  Barco Display Group.
- * All rights reserved.
- *
- * This Software is confidential and proprietary information of Barco Display
- * Group ("Confidential Information"). You shall not disclose such Confidential
- * Information and shall use it only in accordance with the terms of license
- * agreement you entered into with Barco.
- */
-
 /* - module description ---------------------------------------------------- */
 /* - doxygen conformant description about the functionality of this module - */
 /* ######################################################################### */
