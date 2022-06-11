53a5db89e434c23df2d9607cbd46a4902632faaa
hackboxguy <albert.david@gmail.com>
2022-06-11 11:50:28 +0200

    fixed dev-ident led blinking for gl-mt300n-v2 devtype

--- sources/services/sysmgr/srv/src/DevIdentGlMt300nv2.cpp
+++ sources/services/sysmgr/srv/src/DevIdentGlMt300nv2.cpp
@@ -21,28 +21,32 @@ DevIdentGlMt300nv2::~DevIdentGlMt300nv2()
 RPC_SRV_RESULT DevIdentGlMt300nv2::device_identify()
 {
 	char command[255];
-	//blink blue led on nexx3020f pocket router
+	//blink left-green led for 10 seconds on gl-mt300n-v2
+
+	//preserve last state
+	sprintf(command,"cat /sys/class/leds/green:power/brightness > /tmp/last-power-led-state.txt");
+	system(command);
+
 	for(int i=0;i<10;i++)
 	{
-// /sys/class/leds/gl-mt300n-v2\:green\:power/brightness
-
-		sprintf(command,"echo 1 > /sys/class/leds/gl-mt300n-v2\\:green\\:power/brightness");
+		//sprintf(command,"echo 1 > /sys/class/leds/gl-mt300n-v2\\:green\\:power/brightness");
+		sprintf(command,"echo 1 > /sys/class/leds/green:power/brightness");
 		system(command);
 		usleep(250000);
-		sprintf(command,"echo 0 > /sys/class/leds/gl-mt300n-v2\\:green\\:power/brightness");
+		//sprintf(command,"echo 0 > /sys/class/leds/gl-mt300n-v2\\:green\\:power/brightness");
+		sprintf(command,"echo 0 > /sys/class/leds/green:power/brightness");
 		system(command);
 		usleep(250000);
 	}
 
-	//Keep the power LED off
-	sprintf(command,"echo 0 > /sys/class/leds/gl-mt300n-v2\\:green\\:power/brightness");
+	//revert back last state
+	sprintf(command,"echo $(cat /tmp/last-power-led-state.txt) > /sys/class/leds/green:power/brightness");
+	system(command);
+
 	if(system(command)!=0)
 	{
-		//pPanelReq->result=RPC_SRV_RESULT_FILE_OPEN_ERR;
-		//return 0;
 		;
 	}
 	return RPC_SRV_RESULT_SUCCESS;
 }
 /* ------------------------------------------------------------------------- */
-
