ea67d2b3079750079fc3743f2167edb5dbaca957
hackboxguy <albert.david@gmail.com>
2022-06-28 14:53:35 +0200

    fixed brbox-mkuimg multi image creation

--- scripts/brbox-mkuimg.sh
+++ scripts/brbox-mkuimg.sh
@@ -5,6 +5,10 @@ BRBOX_PROJ="BrBoxProj"    #complete project including all sub-modules
 BRBOX_ROOT="BrBoxRtBtr"   #rootfs-baytrail
 BRBOX_STTNG="BrBoxSttng"  #settings partition
 BRBOX_USRDAT="BrBoxUsrDt" #user-data partition
+BRBOX_SIGNATURE="BrBoxSign" #signature file
+BRBOX_GL300NV2="BrBoxGl300N"
+BRBOX_GLA150="BrBoxGlA150"
+
 USAGE="script for generating BrBox binary update packages"
 USAGE1="usage:./$0 -r <rootfs.tar.xz> -v <version.xx.yy> -m <mkimage_tool_path> -t <rootfs_type>"
 ROOTFS_FILE="rootfs.tar.xz"
@@ -13,8 +17,37 @@ OUT_FILE=./uOutFile.uimg
 PACKAGE_HEADER_STRING=BrBox
 ###############################################################################
 #./brbox-mkuimg.sh -r rootfs.tar.xz -v 00.02 -o out.uimg -m ../disk_skeleton/usr/sbin/brbox-mkimage
-#./brbox-mkuimg.sh -r gl-mt300nv2-remote-kit.bin -t BrBoxGl300Nv2 -v 00.02 -o out.uimg -m ../disk_skeleton/usr/sbin/brbox-mkimage
+#./brbox-mkuimg.sh -r gl-mt300nv2-remote-kit.bin -t BrBoxGl300N -v 00.02 -o out.uimg -m ../disk_skeleton/usr/sbin/brbox-mkimage
+#./brbox-mkuimg.sh -r gl-mt300nv2-remote-kit.sign -t BrBoxSign -v 00.02 -o sign.uimg -m ../disk_skeleton/usr/sbin/brbox-mkimage
+#./brbox-mkuimg.sh -r gl-mt300nv2.uimg:sign.uimg -t BrBoxProj -v 00.02 -o uProj.uimg -m ../disk_skeleton/usr/sbin/brbox-mkimage
 ###############################################################################
+Create_Project() #$1=input:files $2version-string $3=output-file
+{
+	printf "Creating Project package ................................ "
+	VERSION_STR="$PACKAGE_HEADER_STRING $BRBOX_PROJ V-$2"
+	$MKIMAGE -A x86  \
+		-O linux \
+		-T multi \
+		-C none  \
+		-a 0     \
+		-e 0     \
+		-n "$VERSION_STR" \
+		-d "$1" "$3"
+}
+Create_SignatureImage() #$1=inputfile $2=version-string $3=output-file
+{
+	printf "Creating Signature package  ............................. "
+	VERSION_STR="$PACKAGE_HEADER_STRING $BRBOX_SIGNATURE V-$2"
+	$MKIMAGE -T $BRBOX_SIGNATURE \
+		-C none \
+		-a 0x00000000 \
+		-e 0x00000000 \
+		-n "$VERSION_STR" \
+                -A x86 \
+		-d "$1" "$3" 1>/dev/null 2>/dev/null
+        test 0 -eq $? && echo "[OK]" || { echo "[FAIL]";return 1;}
+	return 0
+}
 CreateBrBoxRoot() #$1=in-rootfs.tar.xz-file $2=VersionString $3=out-file-path
 {
 	printf "Creating Roofs Upgrade package .......................... "
@@ -61,8 +94,12 @@ do
 	t) BRBOX_ROOT=$OPTARG;;
     esac
 done
-if [ $BRBOX_ROOT = "BrBoxGl300Nv2" ]; then
+if [ $BRBOX_ROOT = $BRBOX_GL300NV2 ]; then
 	CreateMipsImage $ROOTFS_FILE $VERSION $OUT_FILE $BRBOX_ROOT
+elif [ $BRBOX_ROOT = $BRBOX_SIGNATURE ]; then
+	Create_SignatureImage $ROOTFS_FILE $VERSION $OUT_FILE
+elif [ $BRBOX_ROOT = $BRBOX_PROJ ]; then
+	Create_Project $ROOTFS_FILE $VERSION $OUT_FILE
 else
 	CreateBrBoxRoot $ROOTFS_FILE $VERSION $OUT_FILE
 fi
