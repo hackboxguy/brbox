9f5c38334dfec175a499693b926f882a3a3d0cc8
hackboxguy <albert.david@gmail.com>
2022-11-15 12:30:22 +0000

    updated raspi1-rbox config to build with br-2018.02.x

--- packages/brbox_libgloox/brbox_libgloox.mk
+++ packages/brbox_libgloox/brbox_libgloox.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BRBOX_LIBGLOOX_VERSION = 1.0.16 #4
+BRBOX_LIBGLOOX_VERSION = 1.0.24 #4
 BRBOX_LIBGLOOX_SOURCE = gloox-$(BRBOX_LIBGLOOX_VERSION).tar.bz2
 BRBOX_LIBGLOOX_SITE = http://camaya.net/download
 BRBOX_LIBGLOOX_INSTALL_STAGING = YES
